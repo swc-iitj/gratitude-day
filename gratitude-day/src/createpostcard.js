a59ddf9618556e6262a1da41766257e3f420b811
Joel Thomas <thomas.2@iitj.ac.in>
2022-03-09 16:54:16 +0530

    created basic setup of app

---
+++ gratitude-day/src/createpostcard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePostCard = () =>{
+    return (
+        <>
+        <h1>Create Post Card</h1>
+        </>
+    )
+}
+
+export default CreatePostCard;
---
+++ gratitude-day/src/memorywall.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Memorywall = () =>{
+    return (
+        <>
+        <h1>Memorywall</h1> 
+        </>
+    )
+}
+
+export default Memorywall;
