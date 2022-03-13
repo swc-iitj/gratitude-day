3a47a9a629aa5d9c2d12522215fa3ce22f941083
Jahnab Dutta <76475829+JahnabDutta@users.noreply.github.com>
2022-03-13 10:27:37 +0530

    postcard

--- gratitude-day/src/createpostcard.js
+++ gratitude-day/src/createpostcard.js
@@ -1,11 +1,284 @@
-import React from 'react'
+import React, { Component, useState, useEffect } from "react";
+import {useNavigate} from "react-router-dom";
+import {
+  Box,
+  Grid,
+  Typography,
+  Container,
+  Paper,
+  TextField,
+  Button,
+  Snackbar,
+} from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
+import topLeftImage from "./Images/border-top 1.png";
+import topRightImage from "./Images/thank you.png";
+import bottomRightImage from "./Images/border-bottom 1.png";
+import swcLogo from "./Images/swc.png";
+import ImageUploading from "react-images-uploading";
 
-const CreatePostCard = () =>{
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const CreatePostCard = () => {
+  //------------------------------------------------------------------------variables-----------------------------------------------------------//
+  const [images, setImages] = useState([]);
+  const maxNumber = 1;
+  const [email, setEmail] = useState("");
+  const [body, setBody] = useState("");
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+
+  //------------------------------------------------------------------------handle variables-----------------------------------------------------------//
+  const handleImageChange = (imageList, _) => {
+    setImages(imageList);
+    console.log(images);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    console.log(email);
+  };
+
+  const handleBodyChange = (e) => {
+    setBody(e.target.value);
+  };
+  const handleSubmit = () => {
+    if (body.length == 0 && images.length == 0) {
+      setOpen(true);
+      return;
+    }
+    const url = images.map((image,_)=>{return(image.data_url)})
+    console.log(url);
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        body: body,
+        image_url: url,
+      })
+    };
+    fetch('to/url',requestOptions)
+    .then(navigate("/"))
+  };
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+  //------------------------------------------------------------------------components-----------------------------------------------------------//
+
+  const ImageUpload = () => {
     return (
-        <>
-        <h1>Create Post Card</h1>
-        </>
-    )
-}
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={handleImageChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+
+          isDragging,
+          dragProps,
+        }) => (
+          // write your building UI
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <div>
+              <Button
+                style={isDragging ? { color: "red" } : null}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                Click or Drop here
+              </Button>
+              &nbsp;
+              <Button onClick={onImageRemoveAll}>Remove Image</Button>
+            </div>
+          </Box>
+        )}
+      </ImageUploading>
+    );
+  };
+  console.log("hello \n my name");
+
+  return (
+    <div
+      style={{
+        backgroundColor: "#c2c2c2",
+        backgroundSize: "100%",
+      }}
+    >
+      <div style={{ padding: "50px" }}></div>
+      <Grid container justifyContent="space-around" alignItems="flex-start">
+        <Grid item xs={6}>
+          <Container component="main" maxWidth="sm">
+            <Paper elevation={2}>
+              <Box
+                sx={{
+                  p: 2,
+                  m: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Grid
+                  container
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  spacing={1}
+                >
+                  <Grid item>
+                    <Typography variant="h5">Welcome Text</Typography>
+                  </Grid>
+                  <Grid item>
+                    <img src={swcLogo} style={{ maxHeight: "80px" }}></img>
+                  </Grid>
+                </Grid>
+                <Grid
+                  container
+                  direction="column"
+                  justifyContent="space-around"
+                  alignItems="stretch"
+                  spacing={2}
+                >
+                  <Grid item>
+                    <TextField
+                      required
+                      fullWidth
+                      label="Email Address"
+                      autoComplete="email"
+                      value={email}
+                      onChange={handleEmailChange}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      fullWidth
+                      label="Body"
+                      multiline
+                      rows={10}
+                      value={body}
+                      onChange={handleBodyChange}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <ImageUpload />
+                  </Grid>
+                  <Grid item>
+                    <Button fullWidth onClick={handleSubmit}>
+                      Submit
+                    </Button>
+                    <Snackbar
+                      open={open}
+                      autoHideDuration={6000}
+                      onClose={handleClose}
+                    >
+                      <Alert
+                        severity="error"
+                        sx={{ width: "100%" }}
+                        onClose={handleClose}
+                      >
+                        Enter either the message or a photo
+                      </Alert>
+                    </Snackbar>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Paper>
+          </Container>
+        </Grid>
+        <Grid item xs={6}>
+          <Box
+            sx={{
+              p: 2,
+              mx: 2,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={{
+                backgroundImage: "radial-gradient(white,#edd16d)",
+                padding: "10px",
+              }}
+            >
+              <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                alignItems="flex-start"
+                spacing={1}
+              >
+                <Grid item>
+                  <img src={topLeftImage} style={{ maxHeight: "300px" }}></img>
+                </Grid>
+                <Grid item>
+                  <img
+                    src={topRightImage}
+                    alt="alt_text"
+                    style={{ maxHeight: "200px" }}
+                  ></img>
+                </Grid>
+              </Grid>
+              <Box sx={{ pt: 10, px: 4, pb: 2 }}>
+                <Typography
+                  style={{
+                    fontFamily: "Gabriola",
+                    fontSize: "x-large",
+                  }}
+                >
+                  {body}
+                </Typography>
+              </Box>
+
+              <Box sx={{ px: 4, pb: 10, alignItems: "center" }}>
+                {images.map((image, _) => {
+                  return (
+                    <img
+                      src={image.data_url}
+                      style={{ maxWidth: "400px" }}
+                    ></img>
+                  );
+                })}
+              </Box>
+
+              <Grid
+                container
+                direction="row"
+                justifyContent="flex-end"
+                alignItems="flex-end"
+                spacing={1}
+              >
+                <Grid item>
+                  <img
+                    src={bottomRightImage}
+                    style={{ maxHeight: "300px" }}
+                  ></img>
+                </Grid>
+              </Grid>
+            </div>
+          </Box>
+        </Grid>
+      </Grid>
+      <div style={{ padding: "50px" }}></div>
+    </div>
+  );
+};
 
 export default CreatePostCard;
